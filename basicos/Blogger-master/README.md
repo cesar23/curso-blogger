12ff5e2cc14267671d931cf384e368dcbe52226e
Cesar Casa Sheyla <solito_203@hotmail.com>
2019-04-19 18:55:14 -0500

    otros

---
+++ basicos/Blogger-master/README.md
@@ -0,0 +1,2 @@
+# Blogger
+## http://www.codesblue.pro
---
+++ basicos/IMPORTANTE.sh
@@ -0,0 +1,18 @@
+https://github.com/CodesBlue
+
+
+
+fuentes: CodesBlue
+
+https://bloggercode-blogconnexion.blogspot.com/2016/03/tag-b-includable-b-include.html
+
+https://www.codedodle.com/search/label/Blogger
+https://www.codedodle.com/2017/02/blogger-template-design-css-styling-and.html
+
+
+mi theme
+https://perros-de-peru.blogspot.com/
+
+
+tehmes:
+https://github.com/zkreations
